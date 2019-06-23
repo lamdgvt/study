fbaf8b8d8a1819d26f7005989c076543699ced63
Lam <491801318@qq.com>
2019-06-23 19:03:27 +0800

    update code

---
+++ webpack.js
@@ -0,0 +1,26 @@
+/**
+ *  webpack
+ *  安装 webpack
+ *  cnpm install webpack -D
+ *  cnpm install webpack-cli -D
+ *
+ *  webpack 开发环境服务
+ *  cnpm install webpack-dev-server -D
+ *
+ *  html-webpack-plugin 插件
+ *  cnpm install html-webpack-plugin -D
+*/
+
+/**
+ *  npx webpack
+ *  构建配置信息文件
+ *  webpack.config.js
+ *  默认寻找 webpack.config.js 文件
+ *  npx webpack --config xxxx.js
+ *  可指定文件进行 npx 构建
+*/
+
+/**
+ *  npx webpack-dev-serverss
+ *  开启开发环境服务
+*/
