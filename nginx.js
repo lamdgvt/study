a6ed554dcc7ea379194e2e4365be83912b56e010
lamdgvt <491801318@qq.com>
2019-06-30 09:19:49 +0800

    update code

---
+++ nginx.js
@@ -0,0 +1,18 @@
+/**
+ *  nginx
+*/
+
+/**
+ *  nginx http 功能
+ *  每个虚拟主机使用一个 server {} 段配置:
+ *  server {
+ *      listen  端口号
+ *      server_name   主机名
+ *      
+ *      location / {
+ *          
+ *          
+ *      }    
+ *  }
+ *  
+*/
