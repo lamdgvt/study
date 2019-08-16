e1437f6226e35da930c043b4b7e7548cedb5aa56
林威 <linwei@yunjiglobal.com>
2019-08-16 11:41:40 +0800

    update code

---
+++ vue.js
@@ -0,0 +1,37 @@
+/**
+ *  Vue.js
+*/
+
+/**
+ *  全局配置
+ *
+ *  silent    取消Vue 所有日志与警告
+ *  Vue.config.silent = true
+ *
+ *  devtools    配置是否允许 vue-devtools 检查代码
+ *  Vue.config.devtools = true
+ *
+ *  errorHandler    渲染和观察期未捕获的处理函数. 可获取错误信息和 Vue 实例
+ *  Vue.config.errorHandler = function(){}
+ *
+ *  warnHandler   Vue 运行时警告时执行的 自定义回调函数. 仅限开发环境
+ *  Vue.config.warnHandler = function(){}
+ *
+ *  ignoredElements   Vue 忽略报错 使用了未注册的组件名称
+ *  Vue.config.ignoredElements = []
+ *
+ *  keyCodes  自定义 v-on 键位别名
+ *  Vue.config.keyCodes = {
+ *    // 不可使用驼峰式
+ *    "media-play-pause": 179,
+ *  }
+ *  <input type="text" @keyup.media-play-pause="method" />
+ *
+ *  performance   设置浏览器开发工具的性能、时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪.
+*/
+
+
+/**
+ * 全局API
+ * 
+*/
