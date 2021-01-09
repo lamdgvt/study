1809fd5c5459dd96526476665f27771ca9a3d8ae
lamdgvt <491801318@qq.com>
2021-01-09 13:19:52 +0800

    update

---
+++ ts.js
@@ -0,0 +1,29 @@
+/**
+ * let 变量: 类型;
+ * let 变量: 类型 = 值
+ *
+ * boolean 类型
+ * number 数字
+ * string 字符串
+ *
+ * 数组
+ * number[] = [1, 2, 3, 4]
+ * Array<number> = [1, 2, 3]
+ * 
+ * 对象
+ * Object 
+ *
+ * Tuple
+ * let x: [string, number]
+ *
+ * enum 枚举
+ * enum Color {Red, Green, Blue}
+ * let c: Color = Color.Green;
+ * 
+ * Any 任意数值
+ * 
+ * Void 当一个函数没有返回值
+ * 它只能赋值 null 和 undefined
+ * 
+ * Null 和 Undefined
+*/
---
+++ ts.ts
@@ -0,0 +1,25 @@
+/**
+ * let 变量: 类型;
+ * let 变量: 类型 = 值
+ * 
+ * boolean 类型
+ * number 数字
+ * string 字符串
+ * 
+ * 数组
+ * number[] = [1, 2, 3, 4]
+ * Array<number> = [1, 2, 3]
+ * 
+ * Tuple
+ * let x: [string, number]
+ * 
+ * enum 枚举
+ * enum Color {Red, Green, Blue}
+ * let c: Color = Color.Green;
+*/
+
+let action: string = '123';
+
+enum Color {Red, Green, Blue}
+let c: Color = Color.Green;
+
